ab44a0ee6536e931a6c6534656383ac853ccddbd
hieuvh3011 <hieu.vh301195@gmail.com>
2021-05-03 18:26:07 +0700

    DEV-02: add auth middleware for message route

--- src/routing/MessageRoute.ts
+++ src/routing/MessageRoute.ts
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
 import * as MessageController from "../controller/MessageController";
-
+import AuthMiddleware from "../middleware/AuthMiddleware";
 // router.use(function timeLog(req, res, next) {
 //   console.log("Time: ", Date.now());
 //   next();
 // });
 
-router.get("/", function (req, res) {
+router.get("/", AuthMiddleware.isAuth, function (req, res) {
   MessageController.onGetAllMessage(req, res);
 });
 
-router.post("/", function (req, res) {
+router.post("/", AuthMiddleware.isAuth, function (req, res) {
   MessageController.onStoreMessage(req, res);
 });
 
